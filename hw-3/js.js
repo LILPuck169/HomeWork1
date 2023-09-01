66dfe9f4584a8c89a1b158a594fb43ce4e2ea1ad
LILPuck169 <lil_puck169@mail.ru>
2023-09-02 02:26:42 +0300

    Домашние задание 3

---
+++ hw-3/js.js
@@ -0,0 +1,70 @@
+// Задача 1
+// let password = "пароль";
+// let storage = prompt("Введите пароль");
+// if (storage === password) {
+//   console.log("Пароль введен верно");
+// } else {
+//   console.log("Пароль введен неправильно");
+// }
+// Задача 2
+// let c = Number(prompt("Введите число"));
+// if (c > 0 && c < 10) {
+//   console.log("Верно");
+// } else {
+//   console.log("Не Верно");
+// }
+// Задача 3
+// let d = Number(prompt("Введите число"));
+// let e = Number(prompt("Введите число"));
+// if (d > 100 || e > 100) {
+//   console.log("Верно");
+// } else {
+//   console.log("Не Верно");
+// }
+// Задача 4
+// let a = "2";
+// let b = "3";
+// alert(Number(a) + Number(b));
+//Задача 5
+let monthNumber = prompt("Введите номер месяца");
+switch (monthNumber) {
+  case "1":
+    console.log("Это зима");
+    break;
+  case "2":
+    console.log("Это зима");
+    break;
+  case "3":
+    console.log("Это весна");
+    break;
+  case "4":
+    console.log("Это весна");
+    break;
+  case "5":
+    console.log("Это весна");
+    break;
+  case "6":
+    console.log("Это лето");
+    break;
+  case "7":
+    console.log("Это лето");
+    break;
+  case "8":
+    console.log("Это лето");
+    break;
+  case "9":
+    console.log("Это осень");
+    break;
+  case "10":
+    console.log("Это осень");
+    break;
+  case "11":
+    console.log("Это осень");
+    break;
+  case "12":
+    console.log("Это зима");
+    break;
+  default:
+    console.log("Такого месяца не существует");
+    break;
+}
