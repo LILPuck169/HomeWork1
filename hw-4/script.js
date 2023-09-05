f41f85d30dcf60ced58a1c734ee96e520fe26e50
LILPuck169 <lil_puck169@mail.ru>
2023-09-05 11:24:18 +0300

    Домашка 4

---
+++ hw-4/script.js
@@ -0,0 +1,22 @@
+// Задача 1
+// for (let i = 0; i < 2; i++) {
+//   console.log("Привет");
+// }
+// Задача 2
+// for (let i = 1; i < 6; i++) {
+//   console.log(i);
+// }
+// Задача 3
+// for (let i = 7; i < 23; i++) {
+//   console.log(i);
+// }
+// Задача 4
+// const obj = {
+//   Коля: 200,
+//   Вася: 200,
+//   Петя: 200,
+// };
+// for (let money in obj) {
+//   console.log("${money}, Зарплата -  ${obj[money]}");
+// }
+// Задача 5
