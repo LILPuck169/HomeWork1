27416f7dff2890e88429f87a20a44676a7186600
LILPuck169 <lil_puck169@mail.ru>
2023-09-10 14:37:59 +0300

    5 и 6 задачи

--- hw-4/script.js
+++ hw-4/script.js
@@ -17,6 +17,45 @@
 //   Петя: 200,
 // };
 // for (let money in obj) {
-//   console.log("${money}, Зарплата -  ${obj[money]}");
+//   console.log(`${money}, Зарплата -  ${obj[money]}`);
 // }
 // Задача 5
+// Задача 5
+// let num = 0;
+// for (i = 1000; i > 50; i /= 2) {
+//   num += 1;
+// }
+// console.log(num);
+
+// Задача 5
+// let num = 0;
+// for (let i = 1000; i >= 50; i = i / 2) {
+//   num = num + 1;
+//   console.log(i);
+// }
+// console.log(num);
+
+// Задача 5 через while
+// let num = 0;
+// let i = 1000;
+// while (i >= 50) {
+//   console.log(i);
+//   i = i / 2;
+//   num++;
+// }
+// console.log(num);
+
+// Задача 5 через do while
+// let num = 0;
+// let i = 1000;
+// do {
+//   console.log(i);
+//   i = i / 2;
+//   num++;
+// } while (i >= 50);
+// console.log(num);
+
+// Задача 6
+// for (let i = 1; i <= 31; i += 7) {
+//   console.log(`Сегодня пятница ${i}-е число. Необходимо подготовить отчет.  `);
+// }
