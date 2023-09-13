b8b9c1587475145996a792dce4106866c21aef9c
LILPuck169 <lil_puck169@mail.ru>
2023-09-13 22:53:39 +0300

    Ничего необычного

--- hw-5/script.js
+++ hw-5/script.js
@@ -96,19 +96,18 @@
 // console.log(circle1.getPerimeter());
 // console.log(circle2.getPerimeter());
 // Задача 8
-
-let number = Number(prompt("Введите номер месяца!"));
-function date(number) {
-  if ((number >= 1 && number <= 2) || number === 12) {
-    return `Зима`;
-  } else if (number >= 3 && number <= 5) {
-    return `Весна`;
-  } else if (number >= 6 && number <= 8) {
-    return `Лето`;
-  } else if (number >= 9 && number <= 11) {
-    return `Осень`;
-  } else {
-    return `Такого месяца нет!`;
-  }
-}
-alert(date(number));
+// let number = Number(prompt("Введите номер месяца!"));
+// function date(number) {
+//   if ((number >= 1 && number <= 2) || number === 12) {
+//     return `Зима`;
+//   } else if (number >= 3 && number <= 5) {
+//     return `Весна`;
+//   } else if (number >= 6 && number <= 8) {
+//     return `Лето`;
+//   } else if (number >= 9 && number <= 11) {
+//     return `Осень`;
+//   } else {
+//     return `Такого месяца нет!`;
+//   }
+// }
+// alert(date(number));
--- hw-6/script.js
+++ hw-6/script.js
@@ -1,7 +1,23 @@
-let products = ["Yaroslav", "Marik", "Kamarik"];
-products[2] = "Валюха";
-products[3] = "Feya"
-console.log(products[0]);
-console.log(products[1]);
-console.log(products[2]);
-console.log(products[3]);
+// let products = ["Yaroslav", "Marik", "Kamarik"];
+// products[2] = "Валюха";
+// products[3] = "Feya"
+// console.log(products[0]);
+// console.log(products[1]);
+// console.log(products[2]);
+// console.log(products[3]);
+
+// const numbs = [1, 5, 2, 6];
+// for (let i = 0; i < numbs.length; i++) {
+//   console.log(numbs[i]);
+// }
+
+// const numbs = [1, 3, 5, 6, 8];
+// numbs.forEach((el, index, arr) => {
+//   console.log(`${index}: ${el}, массив: ${arr}`);
+// });
+
+let letters = "Yaroslav";
+
+let arr = letters.split( '' );
+
+console.log(arr);
