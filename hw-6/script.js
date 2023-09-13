d31dd2d9fddd9ad54225adfa70743972abfe8e0f
LILPuck169 <lil_puck169@mail.ru>
2023-09-13 18:22:20 +0300

    Готовая домашнее задание 5

---
+++ hw-6/script.js
@@ -0,0 +1,7 @@
+let products = ["Yaroslav", "Marik", "Kamarik"];
+products[2] = "Валюха";
+products[3] = "Feya"
+console.log(products[0]);
+console.log(products[1]);
+console.log(products[2]);
+console.log(products[3]);
