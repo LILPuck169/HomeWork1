22020f08c14261c0c8d047f8c32090565285b31b
LILPuck169 <lil_puck169@mail.ru>
2023-09-16 17:56:12 +0300

    Домашнее задание 6 задачи

--- hw-6/script.js
+++ hw-6/script.js
@@ -1,37 +1,90 @@
-// let products = ["Yaroslav", "Marik", "Kamarik"];
-// products[2] = "Валюха";
-// products[3] = "Feya"
-// console.log(products[0]);
-// console.log(products[1]);
-// console.log(products[2]);
-// console.log(products[3]);
 
-// const numbs = [1, 5, 2, 6];
-// for (let i = 0; i < numbs.length; i++) {
-//   console.log(numbs[i]);
+// Задание 1
+// const arr = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 10) {
+//     break;
+//   }
+//   console.log(arr[i]);
 // }
 
-// const numbs = [1, 3, 5, 6, 8];
-// numbs.forEach((el, index, arr) => {
-//   console.log(`${index}: ${el}, массив: ${arr}`);
-// });
+// Задание 1
 
-//Метод split
+// const arr = [1, 5, 4, 10, 0, 3];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === 10) break;
+//   console.log(arr[i]);
+// }
 
-//Метод Sort
-// let letters = ["C", "W", "D", "B", "A"];
-// letters = letters.sort();
-// console.log(letters);
+// Задание 2 - Поиск индекса
+// const arr = [1, 5, 4, 10, 0, 3];
+// console.log(arr.indexOf(4));
+// Задание 2 - вывод по индексу
+// const arr = [1, 5, 4, 10, 0, 3];
+// console.log(arr[2]);
 
-//Метод join()
-// let letters = ["M", "O", "T", "H", "E"];
-// letters = letters.join("");
-// console.log(letters);
+// задание 3
+// let number = [1, 3, 5, 10, 20];
+// number = number.join(" ");
+// console.log(number);
 
-// Метод pop()
-let products = ["Armchair", "Chair", "Table"];
-console.log(products.pop());
-console.log(products);
-// Метод push()
-// Метод unshift()
-// Метод shift()
+// Задание 4 не понял как делать
+
+// Задание 5
+// const arr = [1, 1, 1];
+// arr.push(2, 2, 2);
+// console.log(arr);
+
+// Задание 6
+// let arr = [9, 8, 7, "a", 6, 5];
+// arr = arr.sort();
+// arr.pop();
+// console.log(arr);
+
+// Задание 7
+// let arr = [9, 8, 7, 6, 5];
+// let number = Number(prompt("Угадай число"));
+// if (arr.includes(number)) {
+//   alert("Угадал");
+// } else {
+//   alert("Не угадал");
+// }
+
+// Задание 8
+// let arr = "abcdef";
+// let secondArr = arr.split("");
+// secondArr.reverse();
+// secondArr = secondArr.join('')
+// console.log(secondArr);
+
+// Задание 9 - Не понял как делать
+// let arr = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+// ];
+// for (let item of arr) {
+//   for (let element of item) {
+//     console.log(element);
+//   }
+// }
+
+// Задание 10
+// let arr = [1, 3, 5, 6, 8, 9, 10];
+// for()
+
+// Задание 11
+// const salary = [2, 4, 6, 8, 10];
+// function square(salary) {
+//   let result = salary.map((item) => item ** 2);
+//   console.log(result);
+// }
+// square(salary);
+
+// Задание 12
+// const arr = ["абвгд", "еёжз", "иклмн", "опрст"];
+// function lengthOfWords(arr) {
+//   return arr.map((word) => word.length);
+// }
+// const wordLen = lengthOfWords(arr);
+// console.log(wordLen);
+// Задание 13
