9e293e1c2f91a74e7e0a38581e4504eac742fb7f
LILPuck169 <lil_puck169@mail.ru>
2023-09-21 02:28:53 +0300

    Домашнее задание 7

--- site/script.js
+++ site/script.js
@@ -12,3 +12,26 @@ function date() {
     return alert(`Такого месяца нет!`);
   }
 }
+
+function playGame() {
+  let words = [
+    "Яблоко",
+    "Груша",
+    "Дыня",
+    "Виноград",
+    "Персик",
+    "Апельсин",
+    "Мандарин",
+  ];
+  words.sort(() => Math.random() - 0.5);
+  alert(words.join(", "));
+  let firstWord = prompt("Чему равнялся первый элемент массива?");
+  let lastWord = prompt("Чему равнялся последний элемент массива?");
+  if (firstWord === words[0] && lastWord === words[words.length - 1]) {
+    alert("Поздравляем! Вы угадали оба слова!");
+  } else if (firstWord === words[0] || lastWord === words[words.length - 1]) {
+    alert("Вы были близки к победе!");
+  } else {
+    alert("Вы ответили неверно!");
+  }
+}
